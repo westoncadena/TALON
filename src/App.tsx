33680ddc0e6c1a53457e68600237b42904b7c51f
westoncadena <westoncadena@gmail.com>
2024-03-07 11:55:47 -0600

    Inital Commit, Barebone vite + react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import talonLogo from './assets/talon.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://github.com/users/westoncadena/projects/1" target="_blank">
+          <img src={talonLogo} className="logo talon" alt="Talon logo" />
+        </a>
+      </div>
+      <h1>Vite + React + TALON</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
