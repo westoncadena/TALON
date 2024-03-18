c06f852bb581906424a621e8fd41bad97a6b4f10
Benjamin Dunning <bend@tamu.edu>
2024-03-18 10:21:50 -0500

    successful communication with supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import talonLogo from './assets/talon.png'
+import { createClient } from '@supabase/supabase-js'
+
 import './App.css'
 
+
+const supabase = createClient("https://dbqngripexpmrgndvwpv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicW5ncmlwZXhwbXJnbmR2d3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk3ODM4NTMsImV4cCI6MjAyNTM1OTg1M30.zDim1uDYoEeT5jmwFCWUewYX77c9PO3UXine1m5ixLg");
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [test, setTest] = useState(["start"]);
+
+  useEffect(() => {
+    tryTest();
+    console.log(test);
+  }, []);
+
+  async function tryTest() {
+    console.log(test);
+    const { data, error } = await supabase.rpc('try_test').eq("registry_id", 1);
+    console.log(data);
+    console.log(error);
+    setTest(data);
+  }
 
   return (
     <>
@@ -22,8 +42,7 @@ function App() {
       </div>
       <h1>Vite + React + TALON</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count + 1)}> 
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
