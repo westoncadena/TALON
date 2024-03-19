9805a302997aab83a7194b5b29b0a16c67745c26
westoncadena <westoncadena@gmail.com>
2024-03-19 12:21:40 -0500

    commiting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react  from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@app": resolve(__dirname, "./src"),
+      "@pages": resolve(__dirname, "./src/pages"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@core": resolve(__dirname, "./src/core"),
+      "@layouts": resolve(__dirname, "./src/layouts"),
+    },
+  }
 })
